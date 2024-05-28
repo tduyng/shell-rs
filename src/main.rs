69aeac9196607cec08de0f7aa2635ac38ecb1b00
tduyng <tduyng.dev@gmail.com>
2024-05-28 22:51:20 +0200

    feat: handle missing command

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,25 @@
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+
+        let command = input.trim();
+        if command.is_empty() {
+            continue;
+        }
+
+        if !is_valid_command(command) {
+            println!("{}: command not found", command)
+        }
+    }
+}
+
+fn is_valid_command(_command: &str) -> bool {
+    false
 }
