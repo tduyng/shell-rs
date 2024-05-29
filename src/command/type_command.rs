a83a99455cd1f7eb3bd2c8dbd0ecf3a9fa7aace9
tduyng <tduyng.dev@gmail.com>
2024-05-29 07:43:31 +0200

    feat: print executable file for type command

--- src/command/type_command.rs
+++ src/command/type_command.rs
@@ -1,3 +1,5 @@
+use std::{env, fs, os::unix::fs::PermissionsExt, path::PathBuf};
+
 pub struct TypeCommand {
     command_name: String,
 }
@@ -12,10 +14,32 @@ impl TypeCommand {
 
         if builtins.contains(&self.command_name.as_str()) {
             println!("{} is a shell builtin", self.command_name);
+        } else if let Some(path) = find_in_path(&self.command_name) {
+            println!("{}", path.display());
         } else {
             println!("{} not found", self.command_name);
         }
 
         Ok(())
     }
+}
+
+fn find_in_path(command_name: &str) -> Option<PathBuf> {
+    let path_var = env::var("PATH").unwrap_or_default();
+    let paths = env::split_paths(&path_var);
+
+    for dir in paths {
+        let full_path = dir.join(command_name);
+        if full_path.is_file() && is_executable(&full_path) {
+            return Some(full_path);
+        }
+    }
+
+    None
+}
+
+fn is_executable(path: &PathBuf) -> bool {
+    fs::metadata(path)
+        .map(|metadata| metadata.permissions().mode() & 0o111 != 0)
+        .unwrap_or(false)
 }
